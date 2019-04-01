a6cf9fa104568ac8dc046325ba101fb47a3d4e0a
cmontellanob <carlosmontellano@gmail.com>
2019-04-01 09:22:10 -0400

    Factory

---
+++ Unidad4/SingletonPatternDemo/SingletonPatternDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleObject ob = SingleObject.getInstance();
+            ob.setValor("Objeto 1");
+            ob.ShowMethod();
+            SingleObject ob2 = SingleObject.getInstance();
+            ob2.ShowMethod();
+            SingleObject ob3 = SingleObject.getInstance();
+            ob2.ShowMethod();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unidad4/SingletonPatternDemo/SingletonPatternDemo/SingleObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPatternDemo
+{
+    class SingleObject
+    {
+        private String valor = "";
+        private static SingleObject instance;
+        private SingleObject()
+        {
+            
+        }
+        public void setValor(String v)
+        {
+            valor = v;
+        }
+        public static SingleObject getInstance()
+        {
+            if (instance == null)
+                instance= new SingleObject();
+            
+                return instance;
+        }   
+        public void ShowMethod()
+        {
+            Console.WriteLine("soy singleton con valor {0}",valor);
+        }
+    }
+}
