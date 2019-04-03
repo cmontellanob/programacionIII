98a5c0298c2697c9d4b73ca8fb1fb6a973fe8144
cmontellanob <carlosmontellano@gmail.com>
2019-04-03 07:37:02 -0400

    Facade

---
+++ Unidad4/Facade/Facade/CheckFacade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class CheckFacade
+    {
+      
+            private AvionApi avionAPI;
+            private HotelApi hotelAPI;
+
+            public CheckFacade()
+            {
+                avionAPI = new AvionApi();
+                hotelAPI = new HotelApi();
+            }
+
+            public void buscar(String fechaIda, String fechaVuelta, String origen, String destino)
+            {
+                avionAPI.buscarVuelos(fechaIda, fechaVuelta, origen, destino);
+                hotelAPI.buscarHoteles(fechaIda, fechaVuelta, origen, destino);
+            }
+        
+
+    }
+}
