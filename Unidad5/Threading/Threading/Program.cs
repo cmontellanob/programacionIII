a29f1fecabf8752432d3d817013a9a63bd1ec0f2
cmontellanob <carlosmontellano@gmail.com>
2019-04-09 08:32:35 -0400

    Sockets

--- Unidad5/Threading/Threading/Program.cs
+++ Unidad5/Threading/Threading/Program.cs
@@ -18,17 +18,36 @@ public class ThreadExample
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Procedimiento Hilo: {0}", i);
-              Thread.Sleep(1000);
+              Thread.Sleep(250);
             }
         }
-
-        public static void Main()
+        public static int Factorial(int n)
         {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+                return Factorial(n - 1) * n;
+        }
+        public static void facto()
+        {
+            Console.WriteLine("Introduzca el valor de n");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("el factorial es {0}",Factorial(n));
+        }
+
+        static void Main(string[] args)
+       {
+                
             Console.WriteLine("Hilo Principal : Comienza el segundo hilo.");
             Thread t = new Thread(new ThreadStart(ThreadProc));
-
+            //Creamos la instancia del hilo 
+            Thread t2 = new Thread(new ThreadStart(facto));
+            //Iniciamos el hilo 
+            
             t.Start();
-
+            t2.Start();
             for (int i = 0; i < 40; i++)
             {
                 Console.WriteLine("Hilo Principal haciendo algun trabajo");
