53b025cf9fbd1741197c92997257a0ff5ae80198
cmontellanob <carlosmontellano@gmail.com>
2019-04-24 07:10:45 -0400

    tic tac toe

---
+++ TresEnRaya/TresEnRaya/Raya.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TresEnRaya
+{
+    class Raya
+    {
+        private Char[,] tabla ;
+        private int turno;
+        public  Raya()
+        {
+            turno = 1;
+            tabla = new char[3,3];
+        }
+
+        public String marcar(int x,int y, Char marca)
+        {
+            // verificar si el turno corresponde
+            // si esta vacio marcar
+            // verificar si hy ganador
+            // verificar si hay empate
+            // pasar el turno
+            // 
+
+        }
+        private Char ganador()
+        {
+
+        }
+        private Boolean empate()
+        {
+
+        }
+
+    }
+}
