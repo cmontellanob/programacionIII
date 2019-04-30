279846413a138fe5275b695da5ea823977e3cfb3
cmontellanob <carlosmontellano@gmail.com>
2019-04-30 07:15:05 -0400

    tres en raya

--- TresEnRaya/TresEnRaya/Form1.Designer.cs
+++ TresEnRaya/TresEnRaya/Form1.Designer.cs
@@ -28,13 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pb22 = new System.Windows.Forms.PictureBox();
+            this.pb21 = new System.Windows.Forms.PictureBox();
+            this.pb20 = new System.Windows.Forms.PictureBox();
+            this.pb12 = new System.Windows.Forms.PictureBox();
+            this.pb11 = new System.Windows.Forms.PictureBox();
+            this.pb10 = new System.Windows.Forms.PictureBox();
+            this.pb02 = new System.Windows.Forms.PictureBox();
+            this.pb01 = new System.Windows.Forms.PictureBox();
+            this.pb00 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb00)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pb22);
+            this.panel1.Controls.Add(this.pb21);
+            this.panel1.Controls.Add(this.pb20);
+            this.panel1.Controls.Add(this.pb12);
+            this.panel1.Controls.Add(this.pb11);
+            this.panel1.Controls.Add(this.pb10);
+            this.panel1.Controls.Add(this.pb02);
+            this.panel1.Controls.Add(this.pb01);
+            this.panel1.Controls.Add(this.pb00);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 304);
+            this.panel1.TabIndex = 0;
+            // 
+            // pb22
+            // 
+            this.pb22.Location = new System.Drawing.Point(230, 205);
+            this.pb22.Name = "pb22";
+            this.pb22.Size = new System.Drawing.Size(88, 79);
+            this.pb22.TabIndex = 8;
+            this.pb22.TabStop = false;
+            // 
+            // pb21
+            // 
+            this.pb21.Location = new System.Drawing.Point(125, 205);
+            this.pb21.Name = "pb21";
+            this.pb21.Size = new System.Drawing.Size(88, 79);
+            this.pb21.TabIndex = 7;
+            this.pb21.TabStop = false;
+            // 
+            // pb20
+            // 
+            this.pb20.Location = new System.Drawing.Point(18, 205);
+            this.pb20.Name = "pb20";
+            this.pb20.Size = new System.Drawing.Size(88, 79);
+            this.pb20.TabIndex = 6;
+            this.pb20.TabStop = false;
+            // 
+            // pb12
+            // 
+            this.pb12.Location = new System.Drawing.Point(230, 113);
+            this.pb12.Name = "pb12";
+            this.pb12.Size = new System.Drawing.Size(88, 79);
+            this.pb12.TabIndex = 5;
+            this.pb12.TabStop = false;
+            // 
+            // pb11
+            // 
+            this.pb11.Location = new System.Drawing.Point(125, 113);
+            this.pb11.Name = "pb11";
+            this.pb11.Size = new System.Drawing.Size(88, 79);
+            this.pb11.TabIndex = 4;
+            this.pb11.TabStop = false;
+            // 
+            // pb10
+            // 
+            this.pb10.Location = new System.Drawing.Point(18, 113);
+            this.pb10.Name = "pb10";
+            this.pb10.Size = new System.Drawing.Size(88, 79);
+            this.pb10.TabIndex = 3;
+            this.pb10.TabStop = false;
+            // 
+            // pb02
+            // 
+            this.pb02.Location = new System.Drawing.Point(230, 18);
+            this.pb02.Name = "pb02";
+            this.pb02.Size = new System.Drawing.Size(88, 79);
+            this.pb02.TabIndex = 2;
+            this.pb02.TabStop = false;
+            // 
+            // pb01
+            // 
+            this.pb01.Location = new System.Drawing.Point(125, 18);
+            this.pb01.Name = "pb01";
+            this.pb01.Size = new System.Drawing.Size(88, 79);
+            this.pb01.TabIndex = 1;
+            this.pb01.TabStop = false;
+            // 
+            // pb00
+            // 
+            this.pb00.Location = new System.Drawing.Point(18, 18);
+            this.pb00.Name = "pb00";
+            this.pb00.Size = new System.Drawing.Size(88, 79);
+            this.pb00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb00.TabIndex = 0;
+            this.pb00.TabStop = false;
+            this.pb00.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reiniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Turno:";
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Location = new System.Drawing.Point(57, 15);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(12, 13);
+            this.lblTurno.TabIndex = 3;
+            this.lblTurno.Text = "x";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(13, 132);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(344, 14);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.Location = new System.Drawing.Point(14, 226);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(344, 14);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.Location = new System.Drawing.Point(120, 36);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(15, 283);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.Location = new System.Drawing.Point(226, 32);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(15, 283);
+            this.panel5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(556, 347);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.lblTurno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Juego Tres en Raya";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pb22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb00)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pb22;
+        private System.Windows.Forms.PictureBox pb21;
+        private System.Windows.Forms.PictureBox pb20;
+        private System.Windows.Forms.PictureBox pb12;
+        private System.Windows.Forms.PictureBox pb11;
+        private System.Windows.Forms.PictureBox pb10;
+        private System.Windows.Forms.PictureBox pb02;
+        private System.Windows.Forms.PictureBox pb01;
+        private System.Windows.Forms.PictureBox pb00;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
     }
 }
 
--- TresEnRaya/TresEnRaya/Form1.cs
+++ TresEnRaya/TresEnRaya/Form1.cs
@@ -16,5 +16,25 @@ namespace TresEnRaya
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Raya.getInstancia().Reiniciar();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            String respuesta= Raya.getInstancia().marcar(0,0, lblTurno.Text[0]);
+            if (lblTurno.Text=="X")
+                pb00.Image= new System.Drawing.Bitmap("D:\\X.png");
+            else
+                pb00.Image = new System.Drawing.Bitmap("D:\\O.png");
+        }
     }
 }
