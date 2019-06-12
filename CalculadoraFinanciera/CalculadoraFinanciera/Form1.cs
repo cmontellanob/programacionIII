52e900cfc8804aa62a1346c739d9365d28b8128d
cmontellanob <carlosmontellano@gmail.com>
2019-06-12 08:59:42 -0400

    Calculadora Financiera

---
+++ CalculadoraFinanciera/CalculadoraFinanciera/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraFinanciera
+{
+    public partial class Form1 : Form
+    {
+        public int arriba = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i=0;i<int.Parse(textBox1.Text);i++)
+            {
+                 TextBox txt = new TextBox();
+                txt.Top = arriba;
+                arriba += 22;
+                panel1.Controls.Add(txt);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] flujos =new double[int.Parse(textBox1.Text)];
+            int i = 0;
+            foreach (Control control in panel1.Controls)
+            {
+                if (control.GetType().Name=="TextBox")
+                {
+
+                    flujos[i]=double.Parse(control.Text);
+                    i++;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ CalculadoraFinancieraDroid/CalculadoraFinancieraDroid/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using static Android.App.ActionBar;
+
+namespace CalculadoraFinancieraDroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnObtener;
+        EditText n;
+        LinearLayout textBoxHolder;
+        List<EditText> _TextosEdicion;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnObtener = FindViewById<Button>(Resource.Id.btnObtener);
+            textBoxHolder = FindViewById<LinearLayout>(Resource.Id.lTextos);
+            n= FindViewById<EditText>(Resource.Id.textVN);
+            btnObtener.Click += btnObtener_Click;
+
+            _TextosEdicion = new List<EditText>();
+
+        }
+        void btnObtener_Click(object sender, System.EventArgs e)
+        {
+            for (int i=1;i<=int.Parse(n.Text);i++)
+            {
+                var editText = new EditText(this);
+                editText.Tag = $"Flujo {i}";
+
+                var layoutParams = new LinearLayout.LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent);
+                layoutParams.BottomMargin = 6;
+
+                editText.LayoutParameters = layoutParams;
+
+                editText.Hint = $"Introduzca Flujo # {i}";
+
+                //Solo si quieres guardar referencia de ellos para luego tomar su valor.
+                _TextosEdicion.Add(editText);
+
+                textBoxHolder.AddView(editText);
+
+            }
+
+            
+
+            
+        }
+    }
+}
